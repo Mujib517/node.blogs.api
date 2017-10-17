c2b2c237df704b34360eb960e78090eba9293fbf
Rahman <mr185242@ncr.com>
2017-10-17 15:45:32 +0530

    PORT from env

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var fs = require('fs');
-var bodyParser=require('body-parser');
+var bodyParser = require('body-parser');
 
-var userRouter=require('./routes/user.router');
-var defaultRouter=require('./routes/default.router');
+var userRouter = require('./routes/user.router');
+var defaultRouter = require('./routes/default.router');
 
 var app = express();
 
-app.listen(3000, function () {
+var port = process.env.PORT | 3000;
+
+app.listen(port, function () {
     console.log("Server is running on 3000");
 });
 
 app.use(bodyParser.json());
 
-app.use('/',defaultRouter);
-app.use('/users',userRouter);
+app.use('/', defaultRouter);
+app.use('/users', userRouter);
